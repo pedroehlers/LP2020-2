157cc1fcd568deff575672814aae79581976226f
pedroehlers <80069311+pedroehlers@users.noreply.github.com>
2021-07-27 02:52:48 +0000

    v1

---
+++ TSILP2020-2/SEMANA01_q06.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	int n1;
+	scanf("%i",&n1);
+	if(n1==0){
+		printf("zero");
+	}
+	if(n1==1){
+		printf("one");
+	}
+	if(n1==2){
+		printf("two");
+	}
+	if(n1==3){
+		printf("three");
+	}
+	if(n1==4){
+		printf("four");
+	}
+	if(n1==5){
+		printf("five");
+	}
+	if(n1==6){
+		printf("six");
+	}
+	if(n1==7){
+		printf("seven");
+	}
+	if(n1==8){
+		printf("eight");
+	}
+	if(n1==9){
+		printf("nine");
+	}
+	
+    
+    return 0;
+}
---
+++ TSILP2020-2/SEMANA01_q09.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	int saque;
+	int n100,n50,n20,n10,n5,n2,n1;
+	int r100,r50,r20,r10,r5,r2,r1;
+	
+	printf("Informe o valor do saque\n");
+	scanf("%i",&saque);
+	
+	n100 = saque/100;
+	r100 = saque%100;
+	
+	n50 = r100/50;
+	r50 = r100%50;
+	
+	n20 = r50/20;
+	r20 = r50%20;
+	
+	n10 = r20/10;
+	r10 = r20%10;
+	
+	n5 = r10/5;
+	r5 = r10%5;
+	
+	n2 = r5/2;
+	r2 = r5%2;
+	
+	n1 = r2/1;
+	r1 = r2%1;
+	
+	if(n100>0){
+		printf("%i cedulas de R$100\n",n100);
+	}
+	if(n50>0){
+		printf("%i cedulas de R$50\n",n50);
+	}
+	if(n20>0){
+		printf("%i cedulas de R$20\n",n20);
+	}
+	if(n10>0){
+		printf("%i cedulas de R$10\n",n10);
+	}
+	if(n5>0){
+		printf("%i cedulas de R$5\n",n5);
+	}
+	if(n2>0){
+		printf("%i cedulas de R$2\n",n2);
+	}
+	if(n1>0){
+		printf("%i cedulas de R$1\n",n1);
+	}
+	
+
+	
+	
+   return 0;
+}
